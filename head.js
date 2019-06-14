1d6b4fb4ffdf60d4b4d7d93bee25aff5d2d55728
israelzoch <zoch.cavalheiro@gmail.com>
2019-06-14 17:08:37 -0300

    edição de páginas

---
+++ head.js
@@ -0,0 +1,15 @@
+document.write
+('<meta charset="utf-8">'+
+	'<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">'+
+	'<meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>'+
+	'<link rel="icon" href="../img/favicon-logoz.png" type="image/png">'+
+	'<title>Zoch DEV.</title>'+
+	'<!-- Bootstrap CSS -->'+
+	'<link rel="stylesheet" href="../css/bootstrap.css">'+
+	'<link rel="stylesheet" href="../vendors/linericon/style.css">'+
+	'<link rel="stylesheet" href="../css/font-awesome.min.css">'+
+	'<link rel="stylesheet" href="../vendors/owl-carousel/owl.carousel.min.css">'+
+	'<link rel="stylesheet" href="../css/magnific-popup.css">'+
+	'<link rel="stylesheet" href="../vendors/nice-select/css/nice-select.css">'+
+	'<!-- main css -->'+
+	'<link rel="stylesheet" href="../css/style.css">');
---
+++ rodape.js
@@ -0,0 +1,22 @@
+document.write('<footer class="footer_area">'+
+'<div class="container">'+
+    '<div class="row justify-content-center">'+
+        '<div class="col-lg-12">'+
+            '<div class="footer_top flex-column">'+
+                '<div class="footer_logo">'+
+                    '<a href="home.html">'+
+                        '<img src="../img/logoz.png" alt="">'+
+                    '</a>'+
+                    '<h4>Siga-me nas Redes Sociais</h4>'+
+                '</div>'+
+                '<div class="footer_social">'+
+                    '<a href="#"><i class="fa fa-instagram"></i></a>'+
+                    '<a href="#"><i class="fa fa-linkedin"></i></a>'+
+                    '<!--<a href="#"><i class="fa fa-dribbble"></i></a>'+
+                    '<a href="#"><i class="fa fa-behance"></i></a>-->'+
+                '</div>'+
+            '</div>'+
+        '</div>'+
+    '</div>'+
+'</div>'+
+'</footer>');
